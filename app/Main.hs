71363611d1d39a2e598dfd1b048b7763575f61a3
Illia Mikhnevych <macbook@MacBook-Pro.local>
2024-06-03 11:32:50 +0300

    runtime

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,10 @@
 module Main where
 
+import BF
+
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
+  let program = ",>,[-<+>]<."
+  case runBF program [2, 4] of
+    Just output -> print output
+    Nothing -> putStrLn "Error"
--- lib/BF.hs
+++ lib/BF.hs
@@ -1,3 +1,55 @@
-module BF (BF (..), parseBF') where
+{-# LANGUAGE LambdaCase #-}
 
+module BF (runBF) where
+
+import Control.Monad (forM_, when)
+import Control.Monad.ST
+import Data.Array.ST
+import Data.STRef
 import Parser
+import Tape
+
+runBF :: String -> [Int] -> Maybe [Int]
+runBF source input = do
+  bf <- parseBF' source
+  pure $ runBF' bf input
+
+type Runtime s = (STRef s [Int], STRef s [Int], TapeArray s, STRef s Int)
+
+getRuntime :: [Int] -> ST s (Runtime s)
+getRuntime input = do
+  idx <- newSTRef 0
+  input' <- newSTRef input
+  arr <- newArray (0, 10) 0 :: ST s (TapeArray s)
+  output <- newSTRef [] :: ST s (STRef s [Int])
+  pure (input', output, arr, idx)
+
+runBF' :: [BF] -> [Int] -> [Int]
+runBF' bf input = runST $ do
+  runtime@(_, output, _, _) <- getRuntime input
+
+  act bf runtime
+
+  modifySTRef output reverse
+  readSTRef output
+  where
+    act :: [BF] -> Runtime s -> ST s ()
+    act chunk runtime@(input', output, arr, idx) = forM_ chunk $ \case
+      Inc -> incr idx arr
+      Dec -> decr idx arr
+      GoLeft -> goLeft idx arr
+      GoRight -> goRight idx arr
+      Write -> do
+        val <- readSTRef input'
+        case val of
+          [] -> putVal (-1) idx arr
+          (x : xs) -> putVal x idx arr *> writeSTRef input' xs
+      Read -> do
+        val <- getVal idx arr
+        modifySTRef output (val :)
+      Loop chunk' -> whileM (isTrue idx arr) (act chunk' runtime)
+
+whileM :: (Monad m) => m Bool -> m a -> m ()
+whileM p f = do
+  isit <- p
+  when isit $ f *> whileM p f
